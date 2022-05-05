ace0f69c24fb233dba076e2ced037474427b4a3d
Luis Valencia <45349016+valencia-luis@users.noreply.github.com>
2022-05-04 23:55:08 -0700

    app update

--- app.js
+++ app.js
@@ -47,19 +47,16 @@ async function listDatabases(client){
 
 
 // Static files
-app.use('/', index);
+
 app.use(express.static('public'))
 app.use('/css', express.static(__dirname + 'public/css'))
-app.use('/js', express.static(__dirname + 'public/js'))
-app.use('/img', express.static(__dirname + 'public/img'))
+
 
 // Set Views
 app.set('views', './views')
 app.set('view engine', 'ejs')
 
-app.get("/", (req, res) => {
-    res.render("index");
-  });
+
 
 app.get('/index.ejs', (_req, res) => {
     res.render('index', { text: 'This is EJS!'})
